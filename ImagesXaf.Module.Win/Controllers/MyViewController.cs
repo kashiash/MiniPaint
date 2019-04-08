83980274779f8b1d6ab91cd77ee8361dbbfb1112
Jacek Kosiński <jkosinski@gabos.pl>
2019-04-08 22:24:51 +0200

    po uruchomieniu wersji xaf

--- ImagesXaf.Module.Win/Controllers/MyViewController.cs
+++ ImagesXaf.Module.Win/Controllers/MyViewController.cs
@@ -5,6 +5,7 @@ using DevExpress.ExpressApp.Win.Editors;
 using DevExpress.XtraGrid;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,61 +15,193 @@ namespace ImagesXaf.Module.Win.Controllers
 {
     public class MyViewController : ViewController<DetailView>
     {
+        //protected override void OnActivated()
+        //{
+        //    base.OnActivated();
+
+        //    foreach (ViewItem item in ((DetailView)View).Items)
+        //    {
+        //        if ((item is ControlViewItem) & item.Id == "Item1") { /*...*/ }
+        //    }
+
+
+        //    ImagePropertyEditor imageEditor = View.FindItem("Photo") as ImagePropertyEditor;
+
+
+        //    if (imageEditor != null)
+        //    {
+        //        imageEditor.ControlCreated += (s, e) =>
+        //            {
+
+        //                ImagePropertyEditor cos = (ImagePropertyEditor)s;
+        //                var cos2 = e;
+        //                var ctrl = ((ImagePropertyEditor)s).Control;
+        //                XafPictureEdit pEdit = (XafPictureEdit)ctrl;
+        //                //     XafImageEdit iEdit = (XafImageEdit)ctrl;
+
+        //                pEdit.MouseDown += MouseDown;
+        //                pEdit.MouseUp += MouseUp;
+        //                pEdit.MouseMove += MouseMove;
+        //                pEdit.Invalidated += Invalidated;
+        //            //    pEdit.ControlsCreated += (s1, e1) => { }
+        //                //{
+        //                //    GridListEditor gridListEditor = ((ListView)s1).Editor as GridListEditor;
+        //                //    if (gridListEditor != null)
+        //                //    {
+        //                //        GridControl grid = gridListEditor.Grid;
+        //                //    }
+        //                //};
+        //            };
+
+
+        //    }
+
+        //}
+
+
+        Image mainImage = null;
+        bool startPaint = false;
+        Graphics graphics;
+        //nullable int for storing Null value
+        int? initX = null;
+        int? initY = null;
+        PointF ulCorner;
+        XafPictureEdit pEdit;
+
         protected override void OnActivated()
         {
             base.OnActivated();
+            ((CompositeView)View).ItemsChanged += PictureditorController_ItemsChanged;
+            TryInitializeAnniversaryItem();
+        }
+        protected override void OnDeactivated()
+        {
+            base.OnDeactivated();
+            ((CompositeView)View).ItemsChanged -= PictureditorController_ItemsChanged;
+        }
 
-            foreach (ViewItem item in ((DetailView)View).Items)
+
+        private void PictureditorController_ItemsChanged(Object sender, ViewItemsChangedEventArgs e)
+        {
+            if (e.ChangedType == ViewItemsChangedType.Added && e.Item.Id == "Anniversary")
             {
-                if ((item is ControlViewItem) & item.Id == "Item1") { /*...*/ }
+                TryInitializeAnniversaryItem();
             }
+        }
 
+        public void TryInitializeAnniversaryItem()
+        {
 
             ImagePropertyEditor imageEditor = View.FindItem("Photo") as ImagePropertyEditor;
-            
-
             if (imageEditor != null)
             {
-                imageEditor.ControlCreated += (s, e) =>
-                    {
+                if (imageEditor.Control != null)
+                {
+                    InitPhotoEditor(imageEditor);
+                }
+                else
+                {
+                    imageEditor.ControlCreated += new EventHandler<EventArgs>(imageEditor_ControlCreated);
+                }
+            }
+        }
 
-                        ImagePropertyEditor cos = (ImagePropertyEditor)s;
-                        var cos2 = e;
-                        var ctrl = ((ImagePropertyEditor)s).Control;
-                        XafPictureEdit pEdit = (XafPictureEdit)ctrl;
-                        //     XafImageEdit iEdit = (XafImageEdit)ctrl;
+        private void imageEditor_ControlCreated(object sender, EventArgs e)
+        {
+            InitPhotoEditor((ImagePropertyEditor)sender);
+        }
 
-                        pEdit.MouseDown += MouseDown;
-                        pEdit.MouseUp += MouseUp;
-                        pEdit.MouseMove += MouseMove;
-                    //    pEdit.ControlsCreated += (s1, e1) => { }
-                        //{
-                        //    GridListEditor gridListEditor = ((ListView)s1).Editor as GridListEditor;
-                        //    if (gridListEditor != null)
-                        //    {
-                        //        GridControl grid = gridListEditor.Grid;
-                        //    }
-                        //};
-                    };
+        private void InitPhotoEditor(ImagePropertyEditor imageEditor)
+        {
+            var ctrl = imageEditor.Control;
+            pEdit = (XafPictureEdit)ctrl;
+            if (pEdit != null)
+            {
+                pEdit.MouseDown += MouseDown;
+                pEdit.MouseUp += MouseUp;
+                pEdit.MouseMove += MouseMove;
+                pEdit.Invalidated += Invalidated;
+                pEdit.LoadCompleted += LoadCompleted;
+                pEdit.ImageChanged += ImageChanged;
+                pEdit.Resize += Resize;
 
-         
             }
 
+        }
+
+        private void Resize(object sender, EventArgs e)
+        {
+
+            graphics = pEdit.CreateGraphics();
+            if (graphics != null )
+            {
+                mainImage = pEdit.Image;
+                if (mainImage != null)
+                {
+                    graphics.DrawImage(mainImage, 0, 0, pEdit.Width, pEdit.Height); 
+                }
+            }
+
+        }
+
+        private void ImageChanged(object sender, EventArgs e)
+        {
+
+            if (graphics == null)
+            {
+                graphics = pEdit.CreateGraphics();
+                mainImage = pEdit.Image;
+
+
+                ulCorner = new PointF(0, 0);
+                if (mainImage != null)
+                {
+                    //  graphics.DrawImage(mainImage, ulCorner);
+                    graphics.DrawImage(mainImage, 0, 0, pEdit.Width, pEdit.Height);
+                }
+            }
+        }
+
+        private void LoadCompleted(object sender, EventArgs e)
+        {
+            //    graphics = pEdit.CreateGraphics();
+            //    mainImage = pEdit.Image;
+
+
+            //    ulCorner = new PointF(0, 0);
+            //    graphics.DrawImage(mainImage, ulCorner);
+        }
+
+        private void Invalidated(object sender, InvalidateEventArgs e)
+        {
+            //  throw new NotImplementedException();
         }
 
         private void MouseMove(object sender, MouseEventArgs e)
         {
-            throw new NotImplementedException();
+            if (startPaint && graphics != null)
+            {
+                //Setting the Pen BackColor and line Width
+                Pen p = new Pen(Color.Red, 3);
+                //Drawing the line.
+                graphics.DrawLine(p, new Point(initX ?? e.X, initY ?? e.Y), new Point(e.X, e.Y));
+                initX = e.X;
+                initY = e.Y;
+            }
         }
 
         private void MouseUp(object sender, MouseEventArgs e)
         {
-            throw new NotImplementedException();
+            // startPaint = false;
         }
 
         private void MouseDown(object sender, MouseEventArgs e)
         {
-            throw new NotImplementedException();
+            // startPaint = true;
+
+            //  SolidBrush sb = new SolidBrush(Color.Red);
+            Pen pen = new Pen(Color.Red, 3);
+            graphics.DrawEllipse(pen, e.X - 50, e.Y - 50, 100, 100);
         }
     }
 }
